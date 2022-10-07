44d0b1c2918fe340436154aabe0646e7d3d1eb7e
2DC <kkts9308@gmail.com>
2022-10-07 18:19:44 +0900

    제주 알캠 문제 준비
    테스트케이스를 만들었음.

--- jeju-alcam/cReduce.js
+++ jeju-alcam/cReduce.js
@@ -1,17 +1,42 @@
+allFuns = {}
 
-
-Array.prototype.cReduce = function(cb, acc, i = 0) {
-
-}
+/* 여기서부터 시작! */
 
 
 
-const result = [5,8,7].cReduce( (acc, e, i, arr) => {
-    console.log(acc, e, i, arr)
-    return acc + e + i
-  }, 'hi')
-  // console.log will be called 3 times:
-  //    'hi', 5, 0, original array
-  //    'hi50', 8, 1, original array
-  //    'hi5081', 7, 2, original array
-  // result will be 'hi508172'
+const result = ['one', 'two', 'three'].cReduce( (ac, cv, ci, ar) => {
+  ac[cv] = `${ci + 1} of ${ar.length}`
+  return ac
+}, {})
+
+console.log(result);
+// console.log will be called 3 times:
+//    'hi', 5, 0, original array
+//    'hi50', 8, 1, original array
+//    'hi5081', 7, 2, original array
+// result will be 'hi508172'
+
+
+allFuns.cReduce = Array.prototype.cReduce
+module.exports = allFuns
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Array.prototype.cReduce = function (cb, acc = 0, i = 0) {
+//   if (this.length <= i) return acc
+//   if (acc === 0 && i === 0 && typeof this[i] === 'string') {
+//     acc = ""
+//   }
+//   acc = cb(acc, this[i], i, this)
+//   return this.cReduce(cb, acc, i + 1)
+// }
--- jeju-alcam/cReduce.test.js
+++ jeju-alcam/cReduce.test.js
@@ -1,29 +1,20 @@
-/* global describe it expect */
-
-const solution = require('../9').solution
-solution()
+const solution = require('./cReduce.js')
 
 describe('test cReduce', () => {
-  it('should run callback with 4 parameters', () => {
+  it('콜백함수가 4개의 파라미터를 모두 받아 실행하기', () => {
     let callbacks;
     [9].cReduce((acc, e, i, arr) => {
       callbacks = [acc, e, i, arr]
     }, 8)
     expect(callbacks).toEqual([8, 9, 0, [9]])
   })
-  it('should reduce [1,2,3] to 6', () => {
-    const cb = (ac, cv) => {
-      return ac + cv
-    }
-    expect([1, 2, 3].cReduce(cb, 0)).toEqual(6)
-  })
-  it('should reduce [1,2,3] to 5', () => {
+  it('초기값 -3인 채로 배열 내 모든 숫자를 더함. 이때 요소가 짝수일 때 추가로 +1을 더해줌.', () => {
     const cb = (ac, cv, ci) => {
       return ac + cv + (cv % 2 === 0)
     }
     expect([1, 2, 3].cReduce(cb, -3)).toEqual(4)
   })
-  it('should reduce ["one","two","three"] to { one: "1 of 3", two: "2 of 2", three: "3 of 3" }', () => {
+  it('배열["one","two","three"]을 reduce 메서드를 이용하여 { one: "1 of 3", two: "2 of 2", three: "3 of 3" }로 만들기', () => {
     const cb = (ac, cv, ci, ar) => {
       ac[cv] = `${ci + 1} of ${ar.length}`
       return ac
@@ -32,7 +23,7 @@ describe('test cReduce', () => {
     const b = { one: '1 of 3', two: '2 of 3', three: '3 of 3' }
     expect(a.cReduce(cb, {})).toEqual(b)
   })
-  it('should reduce ["bob", "obo", "boo"] to { bob: true, obo: false, boo: true }', () => {
+  it('배열 ["bob", "obo", "boo"]을 reduce 메서드를 이용해 { bob: true, obo: false, boo: true }로 만들기', () => {
     const cb = (ac, cv, ci) => {
       ac[cv] = (ci % 2 === 0)
       return ac
@@ -42,16 +33,15 @@ describe('test cReduce', () => {
     expect(a.cReduce(cb, {})).toEqual(b)
   })
 
-  it('should reduce the elements of [1,2,3,4] to 10', () => {
+  it('배열의 모든 요소 더하기 with 초기값 입력 안한 상태', () => {
     const cb = (ac, cv) => {
       return ac + cv
     }
     const test = [1, 2, 3, 4]
     expect(test.cReduce(cb)).toEqual(10)
-
   })
 
-  it('should reduce ["1", "2", "3"] to "123"', () => {
+  it('문자열로 구성된 배열 ["1", "2", "3"] 를 더해서 "123" 만들기', () => {
     const cb = (ac, cv) => {
       return ac + cv
     }
