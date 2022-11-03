c8dc98a3b4604a8a06f5bee85e0f8c1db87d9987
2DC <kkts9308@gmail.com>
2022-11-03 23:27:53 +0900

    재귀함수 학습
    재귀함수에 대해 학습했다.

--- 2. problem-solving_pattern/MULTIPLE-POINTER.js
+++ 2. problem-solving_pattern/MULTIPLE-POINTER.js
@@ -5,7 +5,7 @@ Return an array that includes both values
 that sum to zero or undefined if a pair does not exist
 */
 
-sumZero([-3, -2, -1, 0, 1, 2, 3]) // [-3, 3]
+sumZero([-3, -2, -1, 0, 1, 2, 5]) // [-3, 3]
 sumZero([-2, 0, 1, 3]) // undefined
 sumZero([1, 2, 3]) // undefined
 
@@ -28,8 +28,7 @@ function sumZero(arr) {
         
         if(sum === 0) return [arr[left], arr[right]] 
         else if (sum > 0) {
-            console.log(right);
-            right++
+            right--
         }
         else left++
     }
--- 2. problem-solving_pattern/countUniqueValues.js
+++ 2. problem-solving_pattern/countUniqueValues.js
@@ -14,17 +14,6 @@ countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) // 7
 countUniqueValues([]) // 0
 countUniqueValues([-2,-1,-1,0,1]) // 4
 
-
-
-
-
-
-
-
-
-
-
-
 function countUniqueValues(arr){
     if (arr.length === 0) return 0
     let i = 0
