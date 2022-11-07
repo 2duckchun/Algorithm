ce456df79caba371d335819d74cc9f483f3f895a
2DC <kkts9308@gmail.com>
2022-11-07 22:47:03 +0900

    피보나치 수열, 선형 탐색에 대한 학습
    피보나치 수열과 선형 탐색에 대해 학습한 기록을 남김

--- 3. recursion/fibonacci.js
+++ 3. recursion/fibonacci.js
@@ -4,3 +4,9 @@ Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 
 which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
 */
 
+function fibo(num) {
+    if (num <= 2) return 1
+    return fibo(num - 1) + fibo(num - 2)
+}
+
+console.log(fibo(5))
