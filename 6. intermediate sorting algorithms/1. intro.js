8576ff3de5b2ad8a6a8a814bb9c3e54504bbe78a
2DC <kkts9308@gmail.com>
2022-11-14 23:19:47 +0900

    합병정렬에 대한 공부 시작
    합병정렬에 대해 공부를 시작하였음.
    세세하게 나눠서 절반씩 배열을 만들고 비교해보며 만든다는 로직을 알게됨.

---
+++ 6. intermediate sorting algorithms/1. intro.js
@@ -0,0 +1,17 @@
+/*
+Objectives
+* Understand the limitations of the sorting algorithms we've learned so far
+* Implement merge sort
+* implement quick sort
+* implement radix sort
+
+WHY LEARN THIS?
+* The sorting algorithms we've learned so far don't scale well
+* Try out bubble sort on an array of 100000 elements, it will take quite some time!
+
+FASTER SORTS
+* There is a family of sorting algorithms that can improve time complexity from O(n2) to O(n log n)
+* There's a tradeoff betweeen efficiency and simplicity
+* The more efficient algorithms are much less simple, and generally take longer to understand
+* Let's dive in!
+*/
---
+++ 6. intermediate sorting algorithms/merge-sort.js
@@ -0,0 +1,35 @@
+/* 
+Merge Sort
+* it's a combination of two things - merging and sorting / 사실 3개. 분할, 정렬, 합병
+* Exploits the fact that arrays of 0 or 1 element are always sorted
+* Works by decomposing an array into smaller arrays of 0 or 1 elements, then building up a newly sorted array
+/ 정렬할 배열을 0과 1만 있는 배열로 싹 다 분할해놓는다.
+/ 이후 절반 절반 나눠 정렬을 진행한 후
+/ 절반씩 정렬된 배열을 다시 합쳐서 정렬한다.
+*/
+
+/*
+Merging Array
+* in order to implement merge sort, it's useful to first implement a function responsible for merging two sorted arrays
+* Given two arrays which are sorted, this helper function should create a new array which is also sorted, 
+and consists of all of the elements in the two input arrays.
+* This function should run in O(n + m) time and O(n + m) space and should not modify the parameteres passed to it.
+*/
+
+/*
+Merging Arrays Pseudocode
+* Create an empty array, take a look at the smallest values
+in each input array
+* While there are still values we haven't looked at...
+  - if the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to the next value in the first array
+  - if the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
+  - Once we exhaust one array, push in all remaining values from the other array
+
+
+일단 어레이를 합치는 것 부터 알아야겠지?
+두 어레이를 비교할거야.
+첫번째 어레이의 i, j
+*/
+
+
+merge([1, 10, 50], [2, 14, 99, 100])
