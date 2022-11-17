3284ae51f7e0e92ee4d9095ee7f19d04857220db
2DC <kkts9308@gmail.com>
2022-11-17 21:57:24 +0900

    합병정렬 정리
    합병정렬에 대해 정리함

--- 5. sort/sort-basic.js
+++ 5. sort/sort-basic.js
@@ -2,17 +2,15 @@
 배열 내 숫자를 오름차순하는 정렬 알고리즘을 작성하라.
 */
 
-function sort(arr) {
+function naiveBubbleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
             if (arr[j] > arr[j + 1]) {
                 temp = arr[j + 1]
                 arr[j + 1] = arr[j]
                 arr[j] = temp
-                console.log(arr, `비교중: `,arr[j], ', ',arr[j + 1]);
             }   
-        }
-        console.log('다음 루프 실행');        
+        }    
     }
     return arr
 }
--- 6. intermediate sorting algorithms/merge-sort.js
+++ 6. intermediate sorting algorithms/merge-sort.js
@@ -26,10 +26,48 @@ in each input array
   - Once we exhaust one array, push in all remaining values from the other array
 
 
-일단 어레이를 합치는 것 부터 알아야겠지?
-두 어레이를 비교할거야.
-첫번째 어레이의 i, j
+
 */
 
+function merge(arr1, arr2) {
+  let results = []
+  let i = 0
+  let j = 0
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      results.push(arr1[i])
+      i++
+    } else {
+      results.push(arr2[j])
+      j++
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i])
+    i++
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j])
+    j++
+  }
+  return results
+}
 
 merge([1, 10, 50], [2, 14, 99, 100])
+
+
+/*
+mergeSort Pseudocode
+* Break up the array into halves until you have arrays that are empty or have one element.
+* Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
+* Once the array has been merged back together, return the merged (and sorted!) array
+
+*/
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr
+  let mid = Math.floor(arr.length/2)
+  let left = mergeSort(arr.slice(0, mid))
+  let right = mergeSort(arr.slice(mid))
+  return merge(left, right)
+}
