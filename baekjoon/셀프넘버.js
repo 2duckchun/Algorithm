004d319378a2ee8ddab71f82bfc5b98afa3944d3
2DC <kkts9308@gmail.com>
2023-01-30 23:10:45 +0900

    백준 셀프넘버 문제수정

--- baekjoon/셀프넘버.js
+++ baekjoon/셀프넘버.js
@@ -1,20 +1,44 @@
-const map = new Map(new Array(10000).fill(0).map((e, i) => [i + 1, true]))
+// const map = new Map(new Array(10000).fill(0).map((e, i) => [i + 1, true]))
+
+// function d(n) {
+//     let number = n;
+//     let result = 0;
+//     for (let i = 0; i < String(n).length; i++) {
+//         result += number % 10
+//         number = Math.floor(number/10)
+//     }
+//     return n + result
+// }
+
+// for (let i = 1; i <= 10000; i++) {
+//     let noself = d(i)
+//     map.delete(noself)
+// }
+
+// for (let self of map.keys()) {
+//     console.log(self)
+// }
 
 function d(n) {
-    let number = n;
-    let result = 0;
-    for (let i = 0; i < String(n).length; i++) {
-        result += number % 10
-        number = Math.floor(number/10)
+    let result = n
+    let left = 0
+    while (n) {
+        left += n % 10
+        n = Math.floor(n / 10)
     }
-    return n + result
+    return result + left
 }
 
-for (let i = 1; i <= 10000; i++) {
-    let noself = d(i)
-    map.delete(noself)
+function solution(number) {
+    const map = new Map(new Array(10000).fill(0).map((_, i) => [i + 1, true]))
+    let answer = ""
+    for (let i = 1; i <= number; i++) {
+        map.delete(d(i))
+    }
+    for (const key of map.keys()) {
+        answer += key + "\n"
+    }
+    return answer
 }
 
-for (let self of map.keys()) {
-    console.log(self)
-}
+console.log(solution(10000))
