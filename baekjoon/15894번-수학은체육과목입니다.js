caedb65c6da112ce24a329c1791686e539d3cfb2
2DC <kkts9308@gmail.com>
2023-05-15 18:48:31 +0900

    백준 15894 수학은체육과목입니다

---
+++ baekjoon/15894번-수학은체육과목입니다.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "text.txt";
+const num = parseInt(fs.readFileSync(filePath).toString().trim());
+console.log(num * 4);
