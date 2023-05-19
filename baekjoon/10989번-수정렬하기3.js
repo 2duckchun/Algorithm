ec5410b8ccbd789f53ee3633fdc4a3636a67ca24
2DC <kkts9308@gmail.com>
2023-05-19 16:52:44 +0900

    백준 수정렬하기 2, 3

--- baekjoon/10989번-수정렬하기3.js
+++ baekjoon/10989번-수정렬하기3.js
@@ -3,6 +3,17 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "text.txt";
 const newLine = process.platform === "linux" ? "\n" : "\r\n";
 const input = fs.readFileSync(filePath).toString().trim().split(newLine).map(Number).slice(1);
 
-console.log(input.sort((a, b) => a - b).join("\n"));
+const array = Array(10000).fill(0);
 
-console.log([a, g, g, x, b, c, d, k].sort((a, b) => a - b));
+for (let i = 0; i < input.length; i++) {
+  let num = input[i];
+  if (num !== 0) {
+    array[num - 1] += 1;
+  }
+}
+
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array[i]; j++) {
+    console.log(i + 1);
+  }
+}
