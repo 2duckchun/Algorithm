ada64dfcb7666dc72231d9dbbe9aa2e1885ec3bb
2DC <92588154+2duckchun@users.noreply.github.com>
2023-05-23 10:17:47 +0900

    [Silver IV] Title: 다음 소수, Time: 288 ms, Memory: 10984 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.js
+++ 백준/Silver/4134. 다음 소수/다음 소수.js
@@ -5,25 +5,23 @@ const numArr = fs.readFileSync(filePath).toString().trim().split(newLine).map(Nu
 
 function isPrime(num) {
   if (num <= 1) return false;
-  if (num % 2 === 0) {
-    return num === 2 ? true : false;
-  }
-  for (let i = 3; i <= Math.sqrt(num); i = i + 2) {
+  if (num % 2 === 0) return num === 2 ? true : false;
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
     if (num % i === 0) return false;
   }
   return true;
 }
 
 function solution(numArr) {
-  let answer = "";
+  let result = "";
   for (let num of numArr) {
     let temp = num;
     while (isPrime(temp) === false) {
       temp++;
     }
-    answer += `${temp}\n`;
+    result += `${temp}\n`;
   }
-  return answer;
+  return result;
 }
 
 console.log(solution(numArr).trim());
