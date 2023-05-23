1364de8df455ea0572bb5d5e83ef2cda1c4c309a
2DC <92588154+2duckchun@users.noreply.github.com>
2023-05-23 13:15:08 +0900

    [Bronze I] Title: 팰린드롬수, Time: 116 ms, Memory: 9424 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
@@ -4,23 +4,20 @@ const newLine = process.platform === "linux" ? "\n" : "\r\n";
 const arr = fs.readFileSync(filePath).toString().trim().split(newLine);
 
 function solution(arr) {
-  for (let num of arr) {
-    let isPel = true;
-    for (let i = 0; i <= Math.floor(num.length / 2); i++) {
-      if (num[0] === "0") {
-        isPel = false;
-        break;
-      }
-      if (num[i] !== num[num.length - 1 - i]) {
-        isPel = false;
-        console.log("no");
-        break;
-      }
-    }
-    if (isPel) {
-      console.log("yes");
-    }
-  }
+  arr.forEach((el) => {
+    palindrome(el);
+  });
 }
 
+function palindrome(str) {
+  if (str[0] === "0") return;
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      console.log("no");
+      return;
+    }
+  }
+  console.log("yes");
+  return;
+}
 solution(arr);
