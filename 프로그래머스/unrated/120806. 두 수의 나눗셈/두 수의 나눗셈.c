80d28c9ea4fc06132de60a0fc833f751109a562d
2DC <92588154+2duckchun@users.noreply.github.com>
2023-12-11 22:49:19 +0900

    [level unrated] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.c
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 
 int solution(int num1, int num2) {
+    // double 타입에 int를 곱하면 int 타입으로 변환된다.
+    // double 타입을 그대로 반환하고 싶다면 1000.0과 같이 형을 변환한 후 곱해줘야 한다.
+    
     int answer = (double)num1 / (double)num2 * 1000; 
     
     return answer;
