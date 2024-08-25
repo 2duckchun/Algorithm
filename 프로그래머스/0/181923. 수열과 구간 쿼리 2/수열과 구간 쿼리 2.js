06d40ec8eaeaef77579ac09bcc5af2cf9f4dffe1
2DC <92588154+2duckchun@users.noreply.github.com>
2024-08-26 06:50:19 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 37.04 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -2,16 +2,11 @@ function solution(arr, queries) {
     const result = []
     for (const [s, e, k] of queries) {
         const partial = []
-        
         for (let i = s; i <= e; i++) {
             if (arr[i] > k) partial.push(arr[i])
         }
-        
         partial.sort((a, b) => a - b)
-        
-        
         if (partial[0]) result.push(partial[0])
-        
         else result.push(-1)
     }
     return result;
