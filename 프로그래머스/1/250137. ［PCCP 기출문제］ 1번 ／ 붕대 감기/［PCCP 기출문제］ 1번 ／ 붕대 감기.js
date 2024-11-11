37350cbb092a300409271b5919f99387ee58ed48
2DC <92588154+2duckchun@users.noreply.github.com>
2024-11-12 00:26:26 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,32 +1,14 @@
 function solution(bandage, health, attacks) {
     const [시전시간, 초당회복량, 추가회복량] = bandage
-    const 공격정보 = new Map()
-    for (const [공격시간, 데미지] of attacks) {
-        공격정보.set(공격시간, 데미지)        
-    }
-    
-    const 마지막공격 = attacks[attacks.length-1][0]
-    let 회복스택 = 0
+    let 마지막공격시간 = 0
     let 현재체력 = health
-    
-    for (let i = 0; i <= 마지막공격; i++) {
-        const is공격시간 = 공격정보.has(i)
-        // 회복타임
-        if (!is공격시간) {
-            회복스택++
-            현재체력 += 초당회복량
-            if (회복스택 === 시전시간) {
-                현재체력 += 추가회복량
-                회복스택 = 0
-            }
-            현재체력 = Math.min(현재체력, health)
-        } else {
-        // 맞는타임
-            현재체력 -= 공격정보.get(i)
-            회복스택 = 0
-        }
+    for (const [공격시간, 피해량] of attacks) {
+        const 시간차 = 공격시간 - 마지막공격시간 - 1
+        const 회복량 = (시간차 * 초당회복량) + (Math.floor(시간차 / 시전시간) * 추가회복량)
+        현재체력 = Math.min(현재체력 + 회복량, health)
+        현재체력 -= 피해량
+        마지막공격시간 = 공격시간
         if (현재체력 <= 0) return -1
     }
-    
     return 현재체력
 }
