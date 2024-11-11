91c08a33f5ba0a668f215b73d1ba91c4db590fc6
2DC <92588154+2duckchun@users.noreply.github.com>
2024-11-12 01:26:25 +0900

    [level 2] Title: 타겟 넘버, Time: 13.81 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,17 +1,16 @@
 function solution(numbers, target) {
     let count = 0;
-    // 넓우탐은 큐를 쓴다.
-    const queue = [{index: 0, sum: 0}]
-    while (queue.length > 0) {
-        const {index, sum} = queue.pop()
+    // 재귀 사용 깊우탐
+    const dfs = (index, sum) => {
         if (index === numbers.length) {
             if (sum === target) {
                 count++
             }
         } else {
-            queue.push({index: index + 1, sum: sum + numbers[index]})
-            queue.push({index: index + 1, sum: sum - numbers[index]})
+            dfs(index + 1, sum + numbers[index])
+            dfs(index + 1, sum - numbers[index])
         }
     }
+    dfs(0, 0)
     return count
 }
