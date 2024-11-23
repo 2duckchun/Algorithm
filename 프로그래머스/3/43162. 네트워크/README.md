2479cc17d71e4673a57a0397b0d170a2b5d7e9fa
2DC <92588154+2duckchun@users.noreply.github.com>
2024-11-23 23:17:13 +0900

    [level 3] Title: 네트워크, Time: 0.41 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,6 +1,6 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=javascript) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 23일 23:16:58
+2024년 11월 23일 23:17:11
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -5,28 +5,29 @@ function solution(n, computers) {
     // 방문 체크 배열
     const visited = new Array(n).fill(false)
     
-    for (let comIdx = 0; comIdx < computers.length; comIdx++) {
+    // 컴퓨터가 있는 1차 배열 순회
+    for (let comIdx = 0; comIdx < computers.length; comIdx++) { 
         if (!visited[comIdx]) {
-            BFS(comIdx, visited, computers)
-            answer++        
+            // 방문하지 않은 컴퓨터에 한해 DFS 시작
+            DFS(comIdx, visited, computers)
+            answer++
         }
     }
     
     return answer
 }
 
-function BFS(start, visited, computers) {
-    const queue = [start]
-    visited[start] = true
+function DFS(comIdx, visited, computers) {
+    // 방문한 컴퓨터는 바로 방문 처리
+    visited[comIdx] = true
     
-    while (queue.length > 0) {
-        const comIdx = queue.shift()
-        
-        for (let netIdx = 0; netIdx < computers.length; netIdx++) {
-            if (!visited[netIdx] && computers[comIdx][netIdx]) {
-                queue.push(netIdx)
-                visited[netIdx] = true
-            }
+    // 컴퓨터의 네트워크 검사
+    for (let netIdx = 0; netIdx < computers.length; netIdx++) {
+        // 컴퓨터와 네트워크 인덱스가 서로 연결되어 있고 (1일 경우)
+        // 해당 네트워크 인덱스에 위치한 컴퓨터를 방문하지 않았을 경우
+        // DFS 시작
+        if (computers[comIdx][netIdx] && !visited[netIdx]) {
+            DFS(netIdx, visited, computers)
         }
     }
 }
