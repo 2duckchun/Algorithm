bd6fd064a5bd89b837b2c4a5ea2d570acc5469ca
2DC <92588154+2duckchun@users.noreply.github.com>
2024-11-25 21:03:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 33.82 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,41 +1,31 @@
 function solution(maps) {
-    const queue = [[0, 0, 1]]; // 초기 위치와 경로 길이 저장 [x, y, distance]
-    const visited = Array.from({ length: maps.length }, () =>
-        new Array(maps[0].length).fill(false)
-    );
-    const coord = [
-        [-1, 0], [0, 1], [1, 0], [0, -1] // 상, 우, 하, 좌
-    ];
-    const maxX = maps.length - 1;
-    const maxY = maps[0].length - 1;
-
+    // BFS
+    const queue = [[0, 0, 1]]
+    const coord = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    const visited = Array.from({ length: maps.length }, () => new Array(maps[0].length).fill(false))
+    
     const isValidCoord = (x, y) => {
-        return x >= 0 && x <= maxX && y >= 0 && y <= maxY;
-    };
-
+        return x >= 0 && x < maps.length && y >= 0 && y < maps[0].length
+    }
+    
     while (queue.length > 0) {
-        const [x, y, trace] = queue.shift();
-
-        // 목적지 도달 시 최단 경로 반환
-        if (x === maxX && y === maxY) {
-            return trace;
+        const [x, y, trace] = queue.shift()
+        visited[x][y] = true
+        // 종료조건
+        if (x === maps.length - 1 && y === maps[0].length - 1) {
+            return trace
         }
-
-        // 현재 위치에서 동서남북 탐색
+        
+        // x, y를 기준으로 동서남북으로 이동할 수 있는 위치를 구하고 탐색 지속
         for (const [cx, cy] of coord) {
-            const nx = x + cx;
-            const ny = y + cy;
-
-            if (
-                isValidCoord(nx, ny) && // 유효 좌표인지 확인
-                maps[nx][ny] === 1 && // 벽인지 확인
-                !visited[nx][ny] // 방문 여부 확인
-            ) {
-                visited[nx][ny] = true; // 큐에 추가할 때 방문 처리
-                queue.push([nx, ny, trace + 1]);
+            const nx = x + cx
+            const ny = y + cy
+            if (isValidCoord(nx, ny) && maps[nx][ny] === 1 && !visited[nx][ny]) {
+                visited[nx][ny] = true
+                queue.push([nx, ny, trace + 1])
             }
         }
     }
-
-    return -1; // 목적지에 도달할 수 없는 경우
+    
+    return -1
 }
