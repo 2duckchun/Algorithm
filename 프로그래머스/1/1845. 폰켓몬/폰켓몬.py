27c649d27afc29b16c5aa2836c4c40803c44561b
2DC <92588154+2duckchun@users.noreply.github.com>
2026-09-06 22:01:28 +0900

    [level 1] Title: 폰켓몬, Time: 0.55 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -2,6 +2,4 @@ def solution(nums):
     kind = len(set(nums))
     half = len(nums) // 2
     result = 0
-    if kind > half: result = half
-    else: result = kind
-    return result
+    return min(kind, half)
